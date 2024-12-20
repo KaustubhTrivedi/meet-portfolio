0fd39381509a492445f16c1579956f41cda373d0
Kaustubh Trivedi <kaus12tri@gmail.com>
2024-12-20 12:24:15 +0000

    Testing branch rules

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-6xl">Hello Meet's Portfolio</h1>
+      <h1 className="text-6xl">Testing branch rules</h1>
     </>
   );
 }

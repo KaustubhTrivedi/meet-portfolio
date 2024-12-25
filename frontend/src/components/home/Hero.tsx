bf635fed815d2bcce7340fee8626d916d180fce7
Kaustubh Trivedi <kaus12tri@gmail.com>
2024-12-25 20:27:48 +0000

    removed temporary stuff

--- frontend/src/components/home/Hero.tsx
+++ frontend/src/components/home/Hero.tsx
@@ -29,29 +29,34 @@ const Hero = () => {
       variants={container}
       initial="hidden"
       animate="show"
-      className="text-white text-center py-12 sm:py-16 md:py-24 lg:py-32"
+      className="text-white text-center py-12 sm:py-16 md:py-24 lg:py-32 space-y-20"
     >
-      <motion.h1
-        variants={item}
-        className="text-[32px] sm:text-[42px] md:text-[56px] lg:text-[72px] 
-                   font-poppins font-semibold 
-                   leading-[48px] sm:leading-[63px] md:leading-[84px] lg:leading-[108px] 
-                   mb-3 sm:mb-4 md:mb-5 lg:mb-6 
-                   tracking-wide bg-gradient-to-r from-white to-gray-400 
-                   bg-clip-text text-transparent"
-      >
-        Hi I'm Meet Shah
-      </motion.h1>
-      <motion.h1
-        variants={item}
-        className="text-[24px] sm:text-[32px] md:text-[42px] lg:text-[56px] 
-                   font-poppins font-semibold 
-                   leading-[36px] sm:leading-[48px] md:leading-[63px] lg:leading-[84px] 
-                   tracking-wide bg-gradient-to-r from-white to-gray-400 
-                   bg-clip-text text-transparent"
-      >
-        Welcome to my websit, new change
-      </motion.h1>
+      <div>
+        <motion.h1
+          variants={item}
+          className="text-[32px] sm:text-[42px] md:text-[56px] lg:text-[72px] 
+        font-poppins font-semibold 
+        leading-[48px] sm:leading-[63px] md:leading-[84px] lg:leading-[108px] 
+        mb-3 sm:mb-4 md:mb-5 lg:mb-6 
+        tracking-wide bg-gradient-to-r from-white to-gray-400 
+        bg-clip-text text-transparent"
+        >
+          Hi I'm Meet Shah
+        </motion.h1>
+        <motion.h1
+          variants={item}
+          className="text-[24px] sm:text-[32px] md:text-[42px] lg:text-[56px] 
+        font-poppins font-semibold 
+        leading-[36px] sm:leading-[48px] md:leading-[63px] lg:leading-[84px] 
+        tracking-wide bg-gradient-to-r from-white to-gray-400 
+        bg-clip-text text-transparent"
+        >
+          Welcome to my websit
+        </motion.h1>
+      </div>
+      <motion.p className="font-poppins font-medium text-2xl text-[#CFEF00]">
+        Elevating User Experience, one Design at a time.
+      </motion.p>
     </motion.div>
   );
 };

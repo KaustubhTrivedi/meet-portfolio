6879fdfe4eb68bc8a7db553743433a0e0e7bd49e
Kaustubh Trivedi <kaus12tri@gmail.com>
2024-12-28 00:51:17 +0000

    added card carousel component

--- frontend/src/components/home/Explore.tsx
+++ frontend/src/components/home/Explore.tsx
@@ -1,4 +1,11 @@
+import CardCarousel from "./CardCarousel";
+
 const Explore = () => {
-  return <div>testing deployment script...</div>;
+  return (
+    <div>
+      <h2 className="text-3xl text-[#CFEF00]">Explore my work</h2>
+      <CardCarousel />
+    </div>
+  );
 };
 export default Explore;
